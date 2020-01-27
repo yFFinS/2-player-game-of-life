ab97f3cebab53267c38f93cf183a9c0fa0d993c9
yFFinS <43855058+yFFinS@users.noreply.github.com>
2020-01-27 16:46:14 +0500

    added basic game rules

--- gameobjects.py
+++ gameobjects.py
@@ -2,50 +2,67 @@ import pygame as pg
 
 
 class Node(pg.sprite.Sprite):
-    color = {1: pg.Color("red"), 2: pg.Color("blue"), 3: pg.Color("green")}
+    color = {0: pg.Color("black"), 1: pg.Color("blue"), 2: pg.Color("red")}
 
-    def __init__(self, x, y, size, is_alive=False, player=1, group=None):
+    def __init__(self, x, y, size, group=None):
         super().__init__()
-        if group:
+        if group is not None:
             self.add(group)
-        self.is_alive = is_alive
-        self.player = player
+        self.is_alive = False
         self.image = pg.Surface(size)
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
 
+    def change(self, player=1):
+        self.is_alive = not self.is_alive
+        if self.is_alive:
+            self.image.fill(Node.color[player])
+        else:
+            self.image.fill(Node.color[0])
+
+    def get_pos(self):
+        return self.rect.topleft
+
 
 class Grid:
-    def __init__(self, x, y, width=0, height=0, node_size=(30, 30)):
+    NEIGHBOURS = [(0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1)]
+    CYCLIC = 1
+    NONCYCLIC = 2
+
+    def __init__(self, x: int, y: int, grid_size=(1, 1), node_size=(30, 30)) -> None:
+        self.current_player = 1
         self.x = x
         self.y = y
-        self.width = width
-        self.height = height
+        self.grid_size = grid_size
         self.node_size = node_size
-        self.node_group = pg.sprite.Group()
-        self.lines_group = self.create_grid_lines()
-        self.grid = [[Node(x + i * node_size[0], y + j * node_size[1], node_size, self.node_group)
-                      for j in range(height)] for i in range(width)]
+        self.game_mode = Grid.CYCLIC
+        self.nodes = pg.sprite.Group()
+        self.lines = self.__create_grid_lines()
+        self.grid = [[Node(x + i * node_size[0], y + j * node_size[1], node_size, self.nodes)
+                      for j in range(self.grid_size[1])] for i in range(self.grid_size[0])]
 
-    def render(self, screen):
-        self.node_group.draw(screen)
-        self.lines_group.draw(screen)
+    def __change_node(self, node):
+        node.change(self.current_player)
 
-    def create_grid_lines(self) -> pg.sprite.Group:
+    def __collide(self, x, y):
+        return 0 <= x - self.x <= self.grid_size[0] * self.node_size[0]\
+               and 0 <= y - self.y <= self.grid_size[1] * self.node_size[1]
+
+    def __create_grid_lines(self):
         line_color = pg.Color("white")
         group = pg.sprite.Group()
         vertical_line_width = self.node_size[0] // 10
-        vertical_line_length = self.node_size[1] * self.height
+        vertical_line_length = self.node_size[1] * self.grid_size[1]
         horizontal_line_width = self.node_size[1] // 10
-        horizontal_line_length = self.node_size[0] * self.width
-        for x in range(self.width + 1):
+        horizontal_line_length = self.node_size[0] * self.grid_size[0]
+        for x in range(self.grid_size[0] + 1):
             line = pg.sprite.Sprite(group)
             line.image = pg.Surface((vertical_line_width, vertical_line_length))
             line.image.fill(line_color)
             line.rect = line.image.get_rect()
             line.rect.topleft = (x * self.node_size[0] - vertical_line_width // 2, 0)
 
-        for y in range(self.height + 1):
+        for y in range(self.grid_size[1] + 1):
             line = pg.sprite.Sprite(group)
             line.image = pg.Surface((horizontal_line_length, horizontal_line_width))
             line.image.fill(line_color)
@@ -53,3 +70,53 @@ class Grid:
             line.rect.topleft = (0, y * self.node_size[1] - horizontal_line_width // 2)
 
         return group
+
+    def __get_node_at(self, x, y):
+        for node in self.nodes:
+            if node.rect.collidepoint(x, y):
+                return node
+
+    def __get_neighbours(self, node):
+        x, y = node.get_pos()
+        x //= self.node_size[0]
+        y //= self.node_size[1]
+        nb = []
+        for dx, dy in Grid.NEIGHBOURS:
+            if self.game_mode == Grid.NONCYCLIC:
+                x2, y2 = x + dx, y + dy
+                if -1 < x2 < self.grid_size[0] and -1 < y2 < self.grid_size[1]:
+                    nb.append(self.grid[x2][y2])
+            if self.game_mode == Grid.CYCLIC:
+                x2, y2 = (x + dx) % self.grid_size[0], (y + dy) % self.grid_size[1]
+                nb.append(self.grid[x2][y2])
+        return nb
+
+    def __get_node_by_index(self, xi, yi):
+        try:
+            return self.grid[xi][yi]
+        except IndexError:
+            return
+
+    def next_generation(self) -> None:
+        to_change = []
+        for node in self.nodes:
+            nb = self.__get_neighbours(node)
+            alive = len(list(filter(lambda x: x.is_alive, nb)))
+            if node.is_alive and not 2 <= alive <= 3 or not node.is_alive and alive == 3:
+                to_change.append(node)
+        for node in to_change:
+            self.__change_node(node)
+
+    def render(self, screen: pg.Surface) -> None:
+        self.nodes.draw(screen)
+        self.lines.draw(screen)
+
+    def update(self, event: pg.event.EventType) -> None:
+        x, y = event.pos
+        if event.type == pg.MOUSEBUTTONDOWN:
+            if event.button == pg.BUTTON_LEFT:
+                collision = self.__get_node_at(*event.pos)
+                if collision:
+                    self.__change_node(collision)
+
+
--- main.py
+++ main.py
@@ -6,18 +6,25 @@ pg.init()
 width, height = 500, 500
 fps = 60
 
-screen = pg.display.set_mode((width, height))
+screen = pg.display.set_mode((width, height), pg.DOUBLEBUF, pg.RESIZABLE)
 clock = pg.time.Clock()
 running = True
-grid = Grid(0, 0, 10, 10)
+grid_size = (20, 20)
+grid = Grid(0, 0, grid_size, (width // grid_size[0], height // grid_size[1]))
 
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+        if event.type == pg.MOUSEBUTTONDOWN:
+            grid.update(event)
+        if event.type == pg.KEYDOWN:
+            grid.next_generation()
     grid.render(screen)
+
     pg.display.flip()
     clock.tick(fps)
+
     pg.display.set_caption(str(round(clock.get_fps())) + ' FPS')
 
 pg.quit()
